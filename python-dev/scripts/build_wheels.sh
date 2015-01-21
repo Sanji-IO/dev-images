7cb9188002c97d5c8676caefc5a2474b373e7ead
Zack YL Shih <zackyl.shih@moxa.com>
2015-01-21 10:22:24 +0800

    Remove sdk build

--- python-dev/scripts/build_wheels.sh
+++ python-dev/scripts/build_wheels.sh
@@ -13,8 +13,6 @@ for ((index=0; index<${#bundle_array[@]}; index++)); do
     pip install -r /data/bundles/${bundle_array[$index]}/tests/requirements.txt || true
 done
 
-pip install -r /data/libs/sanji/requirements.txt
-
 # packing all packages into whl
 pip wheel `pip freeze` --wheel-dir=$tempdir
 
